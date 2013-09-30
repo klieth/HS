8970f9395b03cc66904970970be6676d14e2b3a9
Kai Lieth <git@radiant-cove-7007>
2013-09-30 18:16:12 -0400

    Changed clock.py

--- Clock/clock.py
+++ Clock/clock.py
@@ -1,3 +1,19 @@
-time = input();
+while True: #we're going to look for times infinitely--I'll quit later.
+    time = input() # Get the time from stdin
+    if time == "0:00": # Quit condition
+        break
+    time = time.split(":") # split on the :, result ["HH","MM"]
+    dm = int(time[1]) * 6 
+    dh = int(time[0]) * 30 + int(time[1])/2.0
 
-time.split(":");
+    # Check out the format strings to see how to add 3 decimal places
+    if dm > dh:
+        out = dm - dh
+        if out > 180:
+            out = 360-out
+        print("{:.3f}".format(out))
+    else:
+        out = dh - dm
+        if out > 180:
+            out = 360-out
+        print("{:.3f}".format(out))
--- Clock/test.py
+++
@@ -1,7 +0,0 @@
-time = input()
-time.split(":")
-timesplit = time.split(":")
-hours = timesplit[0]
-minutes = timesplit[1]
-
-ihours = int(hours)
